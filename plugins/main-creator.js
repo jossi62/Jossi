590c5b7c5349ade44d1f0a1dc05df87e4f712514
alexbotsx <177073716+alexbotsx@users.noreply.github.com>
2025-05-14 20:13:20 -0500

    dxd

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
     m.react('👤')
     let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Alexnn;;\nFN:Alexnn\nORG:Alexnn\nTITLE:\nitem1.TEL;waid=51993047526:51993047526\nitem1.X-ABLabel:Alexnn\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Alexnn\nEND:VCARD`
-    await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] } }, { quoted: m })
+    await conn.sendMessage(m.chat, { contacts: { displayName: 'Alexnn⁩', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['staff']
 handler.tags = ['main']
