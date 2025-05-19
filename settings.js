3ab2a9a15346700d66eac511d0ede3acfd86a281
alexbotsx <177073716+alexbotsx@users.noreply.github.com>
2025-05-18 23:13:03 -0500

    dsad

--- settings.js
+++ settings.js
@@ -26,8 +26,8 @@ global.owner = [
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.mods = [51968914403]
-global.prems = [51968914403]
+global.mods = []
+global.prems = []
 global.APIs = {
   nrtm: 'https://fg-nrtm.ddns.net',
   fgmods: 'https://api.fgmods.xyz',
